ffb422c489e801297f6875096bf476078f473f0b
Samiris De Araujo Santos <sade-ara@c2r2s8.42porto.com>
2025-10-03 16:26:14 +0100

    main validated executer minishell anotation

--- main.c
+++ main.c
@@ -6,7 +6,20 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:48:53 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/10/03 15:48:54 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/10/03 16:25:19 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+int main (int argc, char **argv)
+{
+	if (argc != 1)
+	{
+		// nao deve execurta, pois primeiro deve entrar no shell
+	}
+	else
+	{
+		//inicia o shell
+	}
+	return (0);
+}
