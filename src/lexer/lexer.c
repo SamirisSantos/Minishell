fed50bc270f89ce7ae49f28f105842c5f7e0502a
SamirisSantos <samiris.araujo@gmail.com>
2025-11-12 16:47:59 +0000

    update lexer function and file architecture

---
+++ src/lexer/lexer.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 16:29:34 by sade-ara          #+#    #+#             */
+/*   Updated: 2025/11/12 16:43:55 by sade-ara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+
+/*
+APAGAR | DEL
+Como funciona a leitura de uma linha de comando
+
+cat file.txt | grep "error" > out.txt
+
+t_comd 1:
+  args: ["cat", "file.txt"]
+  outfile_file: NULL
+  next → t_command 2
+
+t_comd 2:
+  args: ["grep", "error"]
+  outfile_file: "out.txt"
+  append: 0
+*/
+
+// criar lista de tokens
+/*
+APAGAR | DEL
+Le a linha de comando
+ -> se tiver espaco em branco no inicio ignorar.
+ -> verificar se 'e um operador
+ -> verificar se 'e um comando
+ -> verificar se 'e um palavra
+ -> verificar infile e outfile
+ -> verificar pipe
+*/
+
+static t_token	*next_token(char **input, t_token **head, t_token *last_token)
+{
+	t_token	*new;
+
+	new = NULL;
+	if (is_metachar(**input))
+		new = handle_operator(input, head);
+	else
+		new = handle_word(input, head, last_token);
+	return (new);
+}
+
+t_token	*lexer(char *input)
+{
+	t_token	*head;
+	t_token	*last_token;
+	t_token	*new;
+
+	head = NULL;
+	new = NULL;
+	last_token = NULL;
+	if (unclosed_quotes(input))
+		return (NULL);
+	while (*input)
+	{
+		while (*input && is_space(*input))
+			input++;
+		if (!*input)
+			break ;
+		new = next_token(&input, &head, last_token);
+		if (!new)
+		{
+			free_tokens(head);
+			return (NULL);
+		}
+		last_token = new;
+	}
+	return (head);
+}
---
+++ src/lexer/unclosed_quotes.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unclosed_quotes.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 16:09:59 by sade-ara          #+#    #+#             */
+/*   Updated: 2025/11/12 16:37:03 by sade-ara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+
+//APAGAR | DELL APAGAR COMENTARIOS
+// SE str == '\0' nao chamar o remove_quotes
+/*
+Pegar a string do lexer, verificar se tem aspas e tira as aspas.
+Procura a primeira aspas varre a string e encontra a segunda aspas.
+e guarda a string sem aspas
+
+exemplo:
+teste de extract_wor
+[1] Chamando extract_word em: ""e"cho name"
+      -> Extraído (Palavra): ""e"cho"
+      -> Ponteiro restante: " name"
+[2] Chamando extract_word em: "name"
+      -> Extraído (Palavra): "name"
+      -> Ponteiro restante: ""
+str = "ec"ho 
+clean_str = echo
+*/
+
+/*
+**  ------------------------------------------------------------------------------------------
+**  | Entrada     | Saída (clean_str) | Bash faz? | Observação                    |
+**  ------------------------------------------------------------------------------------------
+**  | "ec"ho      | echo              | ✅ Sim    | Aspas fechadas corretamente  |
+**  | 'ec"ho'     | ec"ho             | ✅ Sim    | ' protege "                  |
+**  | "ec'ho"     | ec'ho             | ✅ Sim    | " não afeta '                |
+**  | ec"ho       | echo              | ⚠️ Não    | Bash dá erro (aspa aberta)   |
+**  | 'ec"ho      | ec"ho             | ⚠️ Não    | Bash dá erro (aspa aberta)   |
+**  | "e'c"h'o    | e'cho             | ✅ Sim    | Aspas mistas válidas         |
+**  ------------------------------------------------------------------------------------------
+*/
+
+void	quote_flag(char c, char *quote_char)
+{
+	if (*quote_char == 0 && (c == '\'' || c == '"'))
+		*quote_char = c;
+	else if (*quote_char != 0 && c == *quote_char)
+			*quote_char = 0;
+}
+
+int	unclosed_quotes(char *input)
+{
+	char	quote_char;
+	quote_char = 0;
+	while(*input)
+	{
+		quote_flag(*input, &quote_char);
+		input++;
+	}
+	if (quote_char != 0)
+	{
+		printf("Não fechou a aspas"); //APAGAR | DELL
+		// TODO verificar quando nao achar a aspa como se comporta
+		return (1);
+	}
+	return (0);
+}
