faebc68c337d484e0595809e5987235db878f2be
SamirisSantos <samiris.araujo@gmail.com>
2025-11-13 17:34:29 +0000

    review main function

--- main.c
+++ main.c
@@ -6,16 +6,14 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:48:53 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/11/11 17:07:22 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/11/13 17:29:32 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "headers/minishell.h"
 
 /*
-
 readline() → lexer(tokenrização) → parser → executor
-
 */
 
 int	g_exit_status = 0;
@@ -23,10 +21,6 @@ int	g_exit_status = 0;
 int main (int argc, char **argv, char *envp[])
 {
 	t_shell	*shell;
-	int		shell_signal;
-	t_cmd	*cmd;
-	t_token	*tokens;
-	char	*input;
 
 	if (argc != 1)
 	{
@@ -37,22 +31,9 @@ int main (int argc, char **argv, char *envp[])
 	else
 	{
 		init_data(&shell, envp); //TODO
-		while(1)
-		{
-			input = readline("minishell$");
-			if (!input)
-				break;
-			if(shell_signal == EXIT_SIGINT)
-				//TODO
-			if (shell_signal == EXIT_SIGQUIT)
-				/* TODO */
-			tokens = lexer(input);
-			if (syntax_check(tokens)) //TODO
-				continue;
-			cmds = parse_tokens(tokens); //TODO
-			execute(cmds); //TODO
-			free_all(tokens, cmds, input); //TODO
-		}
+		init_signals(); //TODO esta em utils/init.c
+		shell_control(shell); //doing em src ...
+		free_shell(shell); //TODO esta em utils/utils.c
 	}
 	return EXIT_SUCCESS;
 }
