1a28091649c8f2f24fb0b11354b8a565b8dfe371
Samiris De Araujo Santos <sade-ara@c2r1s4.42porto.com>
2025-11-18 16:16:56 +0000

    init function

---
+++ src/inits/init_cmd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_cmd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 16:10:13 by sade-ara          #+#    #+#             */
+/*   Updated: 2025/11/18 16:12:15 by sade-ara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+
+t_cmd	*init_cmd(void)
+{
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	cmd->args = NULL;
+	cmd->input_file = NULL;
+	cmd->output_file = NULL;
+	cmd->append = 0;
+	cmd->heredoc = 0;
+	cmd->next = NULL;
+	return (cmd);
+}
