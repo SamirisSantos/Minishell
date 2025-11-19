8d142fb15438c1951040e8259f39d5c1fdb2afca
Samiris De Araujo Santos <sade-ara@c2r1s2.42porto.com>
2025-11-19 15:25:20 +0000

    update

--- src/expander/expansion.c
+++ src/expander/expansion.c
@@ -6,52 +6,52 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 13:48:47 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/11/12 16:05:56 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/11/19 15:25:10 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-static char	*expand_string(char *str, char **envp)
+static char	*expand_string(char *str, t_shell *shell)
 {
 	char	*new_str;
 	int		final_len;
 
 	if (!str)
 		return (NULL);
-	final_len = get_expanded_len(str, envp);
+	final_len = get_expanded_len(str, shell);
 	new_str = (char *)malloc(sizeof(char) * (final_len + 1));
 	if (!new_str)
 		return (NULL);
-	fill_expanded_str(new_str, str, envp);
+	fill_expanded_str(new_str, str, shell);
 	return (new_str);
 }
 
 void	expand_tokens(t_token *token_list, char **envp)
 {
-	t_token	*curr;
+	t_token	*current;
 	t_token	*prev;
 	char	*expanded_data;
 	char	*clean_delim;
 
-	curr = token_list;
+	current = token_list;
 	prev = NULL;
-	while (curr)
+	while (current)
 	{
 		if (prev && prev->type == HEREDOC)
 		{
 			*clean_delim = remove_quotes(curr->data);
-			free(curr->data);
-			curr->data = clean_delim;
+			free(current->data);
+			current->data = clean_delim;
 		}
-		else if (curr->type == CMD || curr->type == CMD_ARG
-			|| curr->type == REDIR_IN_FILE || curr->type == REDIR_OUT_FILE)
+		else if (current->type == CMD || current->type == CMD_ARG
+			|| current->type == REDIR_IN_FILE || current->type == REDIR_OUT_FILE)
 		{
-			*expanded_data = expand_string(curr->data, envp);
-			free(curr->data);
+			*expanded_data = expand_string(current->data, envp);
+			free(current->data);
 			curr->data = expanded_data;
 		}
-		prev = curr;
-		curr = curr->next;
+		prev = current;
+		current = curr->next;
 	}
 }
