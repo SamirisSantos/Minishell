b3dba19d34ab28fb18d273eb82a2bd2a86b868d6
Samiris De Araujo Santos <sade-ara@c2r1s2.42porto.com>
2025-11-19 16:27:00 +0000

    update

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -6,27 +6,11 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 13:56:33 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/11/19 16:12:07 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/11/19 16:26:06 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
-
-/*
-Como funciona a leitura de uma linha de comando
-
-cat file.txt | grep "error" > out.txt
-
-t_comd 1:
-  args: ["cat", "file.txt"]
-  outfile_file: NULL
-  next → t_command 2
-
-t_comd 2:
-  args: ["grep", "error"]
-  outfile_file: "out.txt"
-  append: 0
-*/
 
 t_token	*new_token(char *value, t_token_type type)
 {
