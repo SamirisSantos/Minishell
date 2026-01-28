1fe673331eefc54f61ece0f6bd59d260be472bdb
SamirisSantos <samiris.araujo@gmail.com>
2026-01-28 17:29:15 +0000

    update

--- src/utils/init.c
+++ src/utils/init.c
@@ -6,12 +6,11 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 14:30:32 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/11/13 16:02:09 by sade-ara         ###   ########.fr       */
+/*   Updated: 2026/01/28 17:04:56 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
-#include "../headers/errors.h"
 
 void	init_data(&shell, envp)
 {
