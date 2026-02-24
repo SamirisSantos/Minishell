d139edd93a5aee6f0ece25ceb8199327a30d002d
SamirisSantos <samiris.araujo@gmail.com>
2026-02-24 14:51:58 +0000

    unset segmention fault correction

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 11:53:56 by cpinho-c          #+#    #+#             */
-/*   Updated: 2026/02/09 15:56:07 by sade-ara         ###   ########.fr       */
+/*   Updated: 2026/02/24 14:49:56 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,39 @@ void	ft_fix_envp(t_shell *shell, int j)
 	}
 }
 
+static void	unset_var(t_shell *shell, char *var)
+{
+	int		j;
+	size_t	var_len;
+
+	j = 0;
+	var_len = ft_strlen(var);
+	while (shell->envp_cpy[j])
+	{
+		if (ft_strncmp(shell->envp_cpy[j], var, var_len) == 0
+			&& shell->envp_cpy[j][var_len] == '=')
+		{
+			free(shell->envp_cpy[j]);
+			ft_fix_envp(shell, j);
+			break ;
+		}
+		j++;
+	}
+}
+
 void	ft_unset(t_shell *shell, char **cmd_args)
 {
 	int		i;
 	int		j;
 	size_t	var_len;
 
+	if (!cmd_args)
+	{
+		shell->exit_status = 0;
+		return ;
+	}
 	i = 1;
 	while (cmd_args[i])
-	{
-		j = 0;
-		var_len = ft_strlen(cmd_args[i]);
-		while (shell->envp_cpy[j])
-		{
-			if (ft_strncmp(shell->envp_cpy[j], cmd_args[i], var_len) == 0 
-				&& shell->envp_cpy[j][var_len] == '=')
-			{
-				free(shell->envp_cpy[j]);
-				ft_fix_envp(shell, j);
-				break ;
-			}
-			j++;
-		}
-		i++;
-	}
+		unset_var(shell, cmd_args[i++]);
 	shell->exit_status = 0;
 }
